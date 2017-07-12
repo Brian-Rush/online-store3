b720dfb90fa7d82cd5f657c4ca528c73d41ea0aa
Andrew <expandrew@gmail.com>
2017-07-12 11:50:57 -0700

    follow tutorial through https://www.learnhowtoprogram.com/javascript/angular-extended/firebase-updating-entries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AdminComponent } from './admin/admin.component';
 import { PipenamePipe } from './pipename.pipe';
+import { EditAlbumComponent } from './edit-album/edit-album.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -30,7 +31,8 @@ export const firebaseConfig = {
     MarketplaceComponent,
     AlbumDetailComponent,
     AdminComponent,
-    PipenamePipe
+    PipenamePipe,
+    EditAlbumComponent
   ],
   imports: [
     BrowserModule,
